085bb6404b09cf0902238f328fe0079c8333fbdc
Kevin Meziere <kmeziere@jpcatholic.edu>
2023-05-25 14:10:48 -0700

    Initial Commit

---
+++ share-mount/share_mountApp.swift
@@ -0,0 +1,17 @@
+//
+//  share_mountApp.swift
+//  share-mount
+//
+//  Created by Kevin Meziere on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct share_mountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
